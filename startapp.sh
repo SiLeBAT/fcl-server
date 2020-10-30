50f9aebfd5b26ce6223d0da906517c91c49fb13c
Birgit Lewicki <lewicki.birgit@gmail.com>
2020-10-30 14:53:15 +0100

    Fix environment variables
    Change-Id: I711cb6a855cdc311f15bd0a66548921ad2b3f640

--- startapp.sh
+++ startapp.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+source ./environment.sh
+
 LOG_FILE=$1
 if [ "$#" -ne 1 ]; then
   LOG_FILE=./fcl_output
 fi
 
-forever -l $LOG_FILE -a start lib/main.js
+BASE_NAME=`basename $LOG_FILE .log`
+DIR_NAME=`dirname $LOG_FILE`
+ADMIN_LOG=$DIR_NAME/$BASE_NAME-admin.log
+
+NODE_ENV=$FCL_NODE_ENV HOST=$FCL_HOST FCL_API_ROOT=$FCL_API_ROOT forever -l $LOG_FILE -a start ./lib/main.js
